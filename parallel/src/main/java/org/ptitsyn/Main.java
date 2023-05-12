342e8f676773310a2fd1f6339d705e948024bc78
Alex-Birdov <114806823+Alex-Birdov@users.noreply.github.com>
2023-05-12 11:29:53 +0300

    small fixes

--- parallel/src/main/java/org/ptitsyn/Main.java
+++ parallel/src/main/java/org/ptitsyn/Main.java
@@ -4,52 +4,51 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) throws Exception{
-        int[] arraySingle = new int[] {11,34,57,70};
-        int[] arrayMulti = new int[] {23,47,64,89};
+        int[] arraySingle = new int[100000];
+        int[] arrayMulti = new int[100000];
 
-        SortWorker worker = new SortWorker();
-        MergeWorker mw = new MergeWorker(arrayMulti, arraySingle);
-        mw.start();
-        mw.join();
+        SingleMergeSort singleMergeSort = new SingleMergeSort();
 
-        System.out.println(Arrays.toString(mw.getMergedArray()));
+        for (int i = 0; i < arraySingle.length; i++) {
+            arraySingle[i] = arrayMulti[i] = (int)(Math.random() * 1000);
+        }
 
-//        SingleMergeSort singleMergeSort = new SingleMergeSort();
+        long startTimeSingle = System.nanoTime();
+        int[] sortedArraySingle = singleMergeSort.mergeSort(arraySingle);
+        long stopTimeSingle = System.nanoTime();
 
-//        for (int i = 0; i < arraySingle.length; i++) {
-//            arraySingle[i] = arrayMulti[i] = (int)(Math.random() * 1000);
-//        }
+        long startTimeMulti = System.nanoTime();
+        SortWorker firstWorker = new SortWorker(arrayMulti);
+        firstWorker.start();
+        try {
+            firstWorker.join();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        long stopTimeMulti = System.nanoTime();
 
-//        long startTimeSingle = System.nanoTime();
-//        int[] sortedArraySingle = singleMergeSort.mergeSort(arraySingle);
-//        long stopTimeSingle = System.nanoTime();
-//
-//        long startTimeMulti = System.nanoTime();
-//        MergeSortWorker firstWorker = new MergeSortWorker(arrayMulti);
-//        firstWorker.start();
-//        try {
-//            firstWorker.join();
-//        }
-//        catch(Exception e) {
-//            e.printStackTrace();
-//        }
-//        long stopTimeMulti = System.nanoTime();
-//
-//        long timeDifference = (stopTimeSingle - startTimeSingle) - (stopTimeMulti - startTimeMulti);
-//        System.out.println("Время, потраченное на выполнение однопоточной сортировки, нс: " + (stopTimeSingle - startTimeSingle));
-//        System.out.println("Время, потраченное на выполнение многопоточной сортировки, нс: " + (stopTimeMulti - startTimeMulti));
-//        System.out.println("Разница времени исполнения: " + timeDifference);
+        long timeDifference = (stopTimeSingle - startTimeSingle) - (stopTimeMulti - startTimeMulti);
+        System.out.println("Время, потраченное на выполнение однопоточной сортировки, нс: " + (stopTimeSingle - startTimeSingle));
+        System.out.println("Время, потраченное на выполнение многопоточной сортировки, нс: " + (stopTimeMulti - startTimeMulti));
+        System.out.println("Разница времени исполнения: " + timeDifference);
 //        System.out.println(Arrays.toString(sortedArraySingle));
 //        System.out.println(Arrays.toString(firstWorker.getSortedArray()));
 
+//        Проверка слияния двух отсортированных массивов
+//        SortWorker worker = new SortWorker();
+//        MergeWorker mw = new MergeWorker(arrayMulti, arraySingle);
+//        mw.start();
+//        mw.join();
+//
+//        System.out.println(Arrays.toString(mw.getMergedArray()));
 
+//      Считываение данных
 //        ReadData readData = new ReadData();
 
+//        Запись данных
 //        WriteData writeArray = new WriteData(5, 20, 200);
 //        writeArray.writeArrays();
 
-//        int[] sortArr = {12, 6, 4, 1, 15, 10};
-//        int[] result = mergeSort(sortArr);
-//        System.out.println(Arrays.toString(result));
     }
 }
--- parallel/src/test/java/org/ptitsyn/MainTest.java
+++ parallel/src/test/java/org/ptitsyn/MainTest.java
@@ -3,6 +3,7 @@ package org.ptitsyn;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class MainTest {
     @Test
     public void testMergeSort() throws Exception{
         SingleMergeSort singleMergeSort = new SingleMergeSort();
-        String path = "C:\\Users\\user\\Documents\\Учеба\\Семестр 2\\Параллельные вычисления\\parallel\\src\\main\\resources";
+        String path = "./src\\main\\resources";
         List <int[]> listOfArrays = readData.readData(path, "test_input.txt");
         List <int[]> sortedListOfArrays  = readData.readData(path, "test_output.txt");
 
